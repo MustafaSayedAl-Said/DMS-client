f6928624ac8af5d5f4c04602ee8895149ad8730d
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2024-08-26 16:33:01 +0300

    Fixed refresh error

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -4,82 +4,86 @@ import { BehaviorSubject, map, of, ReplaySubject } from 'rxjs';
 import { IUser } from '../shared/Models/user';
 import { Router } from '@angular/router';
 import { IWorkspace } from '../shared/Models/Workspaces';
-import { response } from 'express';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-
   _baseURL = 'https://localhost:7030/api/';
   private currentUser = new BehaviorSubject<IUser>(null);
   currentUser$ = this.currentUser.asObservable();
 
-  constructor(private http:HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
-  getCurrentUserValue(){
+  getCurrentUserValue() {
     return this.currentUser.value;
   }
-  loadCurrentUser(token:string){
-    let headers = new HttpHeaders();
-    headers = headers.set('Authorization', 'Bearer ' + token);
-
-    return this.http.get(this._baseURL + 'users/current', {headers}).pipe(
-      map((user:IUser) => {
-        if(user) {
-          localStorage.setItem('token', user.token);
-          this.currentUser.next(user);
-        }
-      })
-    )
-  }
-
-  login(value:any){
-    return this.http.post(this._baseURL + 'users/login', value).pipe(
-      map((user:IUser) => {
-        if(user){
-          localStorage.setItem('token', user.token);
-          this.currentUser.next(user);
-        }
-      })
-    )
-  }
-
-  register(value:any){
-    console.log(value);
-    return this.http.post(this._baseURL + 'users/register', value).pipe(
-      map((user:IUser) => {
-        if(user){
-          localStorage.setItem('token', user.token);
-          this.currentUser.next(user);
-        }
-      })
-    )
-  }
-
-  logout(){
-    localStorage.removeItem('token');
-    this.currentUser.next(null);
-    this.router.navigateByUrl('/')
-  }
-
-  checkEmailExist(email:string){
-    return this.http.get(this._baseURL + 'users/check?email=' + email);
-  }
-
-  getWorkspaceId(){
-    let headers = new HttpHeaders();
-    const token = localStorage.getItem('token');
-    if(token === null){
+  loadCurrentUser(token: string) {
+    if (token === null) {
       this.currentUser.next(null);
+
       return of(null);
     }
+    let headers = new HttpHeaders();
     headers = headers.set('Authorization', 'Bearer ' + token);
-    return this.http.get<IWorkspace>(this._baseURL + 'users/workspace', {headers}).pipe(
-      map(response => {
-        return response.id;
+
+    return this.http.get(this._baseURL + 'users/current', { headers }).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUser.next(user);
+        }
       })
     );
   }
 
+  login(value: any) {
+    return this.http.post(this._baseURL + 'users/login', value).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUser.next(user);
+        }
+      })
+    );
+  }
+
+  register(value: any) {
+    console.log(value);
+    return this.http.post(this._baseURL + 'users/register', value).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUser.next(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUser.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExist(email: string) {
+    return this.http.get(this._baseURL + 'users/check?email=' + email);
+  }
+
+  getWorkspaceId() {
+    let headers = new HttpHeaders();
+    const token = localStorage.getItem('token');
+    if (token === null) {
+      this.currentUser.next(null);
+      return of(null);
+    }
+    headers = headers.set('Authorization', 'Bearer ' + token);
+    return this.http
+      .get<IWorkspace>(this._baseURL + 'users/workspace', { headers })
+      .pipe(
+        map((response) => {
+          return response.id;
+        })
+      );
+  }
 }
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -5,9 +5,9 @@ import {
   Router,
   RouterStateSnapshot,
 } from '@angular/router';
-import { delay, map, Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { AccountService } from '../../account/account.service';
-
+import {jwtDecode} from 'jwt-decode'
 @Injectable({
   providedIn: 'root',
 })
@@ -16,15 +16,46 @@ export class AuthGuard implements CanActivate {
     private accountServices: AccountService,
     private router: Router
   ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> {
     return this.accountServices.currentUser$.pipe(
       map((auth) => {
+        const token = localStorage.getItem('token');
+
         if (auth) {
+          console.log('User is authenticated');
           return true;
         }
+
+        if (token) {
+          try {
+            const decodedToken: any = jwtDecode(token);
+
+            // Check if the token is expired
+            const currentTime = Date.now() / 1000;
+            if (decodedToken.exp < currentTime) {
+              console.log('Token is expired');
+              this.router.navigate(['account/login'], {
+                queryParams: { returnUrl: state.url },
+              });
+              return false;
+            }
+
+            console.log('Token is valid');
+            return true;
+          } catch (error) {
+            console.error('Invalid token format', error);
+            this.router.navigate(['account/login'], {
+              queryParams: { returnUrl: state.url },
+            });
+            return false;
+          }
+        }
+
+        console.log('No user authenticated and no valid token');
         this.router.navigate(['account/login'], {
           queryParams: { returnUrl: state.url },
         });
